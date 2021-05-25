06cc345feecf2232beea5117022e8c8d67f5efa2
NORBERTO <NORBERTO@PC-NORBE>
2021-05-25 15:55:56 +0000

    Modificaciones de los ejercicios 2a y 2b, creación del nuevo proyecto y ajustes en las tabulaciones y formato del código

--- src/figuras/Test.java
+++ src/figuras/Test.java
@@ -3,17 +3,16 @@ package figuras;
 import utilidades.Circulo;
 
 public class Test {
-    public static void main(String[] args)
-{
-        Circulo c1 = new Circulo(5.5);
-        Circulo c2 = new Circulo(10.1);
-        Circulo c3 = new Circulo(10.9);
+	public static void main(String[] args) {
+		Circulo c1 = new Circulo(5.5);
+		Circulo c2 = new Circulo(10.1);
+		Circulo c3 = new Circulo(10.9);
 
-   if (c2.esIgual(c3, false))
-            System.out.println("c2 y c3: iguales sin considerar decimales");
+		if (c2.esIgual(c3, false))
+			System.out.println("c2 y c3: iguales sin considerar decimales");
 
-   if (c2.esIgual(c3, true))
-       System.out.println("c2 y c3: iguales considerando decimales");
-}
+		if (c2.esIgual(c3, true))
+			System.out.println("c2 y c3: iguales considerando decimales");
+	}
 
 }
