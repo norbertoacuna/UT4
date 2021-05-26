91e679324533e51239763f9a210d559b9d9e8939
NORBERTO <NORBERTO@PC-NORBE>
2021-05-26 13:10:11 +0000

    Ejercicio 2m. Deshacer el método calcularArea

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -10,23 +10,18 @@ public class Circunferencia {
 
 	public void imprimir() {
 		color = "rojo";
-		double d = 2 * radio;
-		System.out.println("Diametro: " + d);
+		System.out.println("Diametro: " + (2 * radio));
 		System.out.println("Color: " + color);
 		final double pi = 3.1416;
-		double area = calcularArea(pi);
+		double area1 = 2 * pi * radio * radio;
+		double area = area1;
 		System.out.println(area);
 	}
 
-	private double calcularArea(final double pi) {
-		double area = 2 * pi * radio * radio;
-		return area;
-	}
-
-	public boolean esIgual(Circunferencia otro, boolean conDecimales) {
+	public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
 		double radio1 = this.radio;
 		double radio2 = otro.getRad();
-		if (conDecimales) {
+		if (considerarDecimales) {
 			if (radio1 == radio2)
 				return true;
 			else
